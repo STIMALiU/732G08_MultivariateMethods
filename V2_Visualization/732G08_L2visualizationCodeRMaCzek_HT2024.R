48551a0a47d867cb0a2c70e38675979614475b6e
krzbar <bartoszekkj@gmail.com>
2025-08-08 00:09:22 +0200

    Delete V2_Visualization/732G08_L2visualizationCodeRMaCzek_HT2024.R

--- V2_Visualization/732G08_L2visualizationCodeRMaCzek_HT2024.R
+++
@@ -1,8 +0,0 @@
-library(RMaCzek)
-## code from RMaCzek's manual
-## https://cran.r-project.org/web/packages/RMaCzek/
-# Set data ####
-x<-czek_matrix(mtcars)
-# Standard plot ############
-plot(x)
-plot.czek_matrix(x)
