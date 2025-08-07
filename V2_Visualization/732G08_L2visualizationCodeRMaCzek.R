fc8989cf90bc3f5f3cb47c71fa03c413349f75cc
krzbar <bartoszekkj@gmail.com>
2025-08-08 00:09:40 +0200

    disclaimer

--- V2_Visualization/732G08_L2visualizationCodeRMaCzek.R
+++ V2_Visualization/732G08_L2visualizationCodeRMaCzek.R
@@ -1,3 +1,12 @@
+## This file accompanies the course 732G08 Multivariate Methods 
+
+## This software comes AS IS in the hope that it will be useful WITHOUT ANY WARRANTY, 
+## NOT even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+## Please understand that there may still be bugs and errors. Use it at your own risk. 
+## We take no responsibility for any errors or omissions in this code or for any misfortune 
+## that may befall you or others as a result of its use. Please send comments and report 
+## bugs to Krzysztof Bartoszek at krzbar@protonmail.ch .
+
 library(RMaCzek)
 ## code from RMaCzek's manual
 ## https://cran.r-project.org/web/packages/RMaCzek/
